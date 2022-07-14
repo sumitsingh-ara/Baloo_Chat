d8d45be9f3db739db015979f398df2f67520c8f9
Sumit Singh <sumit47919@gmail.com>
2022-07-14 10:11:01 +0530

    Tab indexing provided to set the focus attribute

--- baloochat/src/Components/BottomNavbar/BottomNavbar.jsx
+++ baloochat/src/Components/BottomNavbar/BottomNavbar.jsx
@@ -1,12 +1,27 @@
-import styles from './BottomNavbar.module.css';
-
-export const BottomNavbar = ( ) =>{
-
-
-
-    return(
-        <div className={styles.bottomContainer}>
-            
+import styles from "./BottomNavbar.module.css";
+import Home from "../../Allicons/Home.svg";
+import Filter from "../../Allicons/Filter.svg";
+import Search from "../../Allicons/Search.svg";
+import User_add from "../../Allicons/User_add.svg";
+export const BottomNavbar = () => {
+  return (
+    <div className={styles.bottomContainer}>
+      <div className={styles.bottomIcons}>
+        <img onClick={()=>{
+            console.log("This is home button,can use to link homepage")
+        }} src={Home} tabIndex={1} alt="" />
+        <img onClick={()=>{
+            console.log("This is filter button,can use to filter chats")
+        }} src={Filter} tabIndex={2} alt="" />
+        <img onClick={()=>{
+            console.log("This is search button,can use to search chats")
+        }} src={Search} tabIndex={3} alt="" />
+      </div>
+      <div className={styles.User_add}>
+        <div className={styles.alignUser}>
+        <img src={User_add} alt="" />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
